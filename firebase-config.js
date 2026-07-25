7d4a744bbbebbd75f078398af713b0332b7a6e88
bellerswang <bellers@hotmail.com>
2026-07-25 22:03:49 +0100

    Configure Firebase web login

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,11 @@
 // Copy the values from Firebase Console > Project settings > Your apps.
 // Never put service-account JSON, OpenAI keys, DeepSeek keys, or workflow keys here.
 window.LOOMI_FIREBASE_CONFIG = {
-    apiKey: 'REPLACE_WITH_FIREBASE_WEB_API_KEY',
+    apiKey: 'AIzaSyBuomy6us4CFDW9C5ZDiEbfgkUGAdiaYcE',
     authDomain: 'mercurial-weft-455321-v6.firebaseapp.com',
     projectId: 'mercurial-weft-455321-v6',
     storageBucket: 'mercurial-weft-455321-v6.firebasestorage.app',
-    messagingSenderId: 'REPLACE_WITH_FIREBASE_MESSAGING_SENDER_ID',
-    appId: 'REPLACE_WITH_FIREBASE_WEB_APP_ID'
+    messagingSenderId: '765248078203',
+    appId: '1:765248078203:web:327ac1238a79268dfbbdde',
+    measurementId: 'G-GS1FNP2XE4'
 };
